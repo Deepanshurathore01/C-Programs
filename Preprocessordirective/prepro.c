d52311bd28659f0c667f35d737b50227b892178c
Deepanshu <deepanshurathore975@gmail.com>
2024-01-17 00:28:56 +0530

    linked list endes here

---
+++ Preprocessordirective/prepro.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#define height 100
+#define num 3.14
+#define letter 'A'
+#define letter_seq "AbC"
+#define bacslashch '\?'
+
+int main()
+{
+  printf("value of height %d",height);
+  printf("value of number %f",num);
+}
